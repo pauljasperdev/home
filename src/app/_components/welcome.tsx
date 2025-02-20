41cb8d43c06c280bee105a429d3ff11b9577df79
pauljaspersahr <pauljasper.sahr@gmail.com>
2025-02-20 11:50:28 +0100

    add shadcn basic && basic style

---
+++ src/app/_components/welcome.tsx
@@ -0,0 +1,7 @@
+export function Welcome() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl">Welcome Traveller</h1>
+    </div>
+  );
+}
