a5ce632b67fb44ac23503a117113c3f272a87600
pauljaspersahr <pauljasper.sahr@gmail.com>
2025-02-21 11:51:54 +0100

    carousel

--- src/app/_components/section.tsx
+++ src/app/_components/section.tsx
@@ -1,3 +1,12 @@
+import { Card, CardContent } from "~/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
+
 const Section = ({
   title,
   description,
@@ -6,9 +15,32 @@ const Section = ({
   description: string;
 }) => {
   return (
-    <div className="flex h-96 flex-col items-center justify-center gap-4">
+    <div className="group flex h-96 flex-col items-center justify-center gap-4">
       <h2 className="text-2xl font-bold">{title}</h2>
       <p className="text-sm text-gray-500">{description}</p>
+      <Carousel
+        className="w-full max-w-[90%]"
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden group-hover:flex" />
+        <CarouselNext className="hidden group-hover:flex" />
+      </Carousel>
     </div>
   );
 };
