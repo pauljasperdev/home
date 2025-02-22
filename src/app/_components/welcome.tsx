59d68adb495f34dafb88f6e53b9ad53ca44eeb54
pauljaspersahr <pauljasper.sahr@gmail.com>
2025-02-22 14:05:38 +0100

    update welcome with twitter

--- src/app/_components/welcome.tsx
+++ src/app/_components/welcome.tsx
@@ -1,7 +1,32 @@
+import { Button } from "~/components/ui/button";
+import Xicon from "./Xicon";
+
 export default function Welcome() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl">Welcome Traveller</h1>
+      <div className="flex flex-col items-start justify-center space-y-8">
+        <h1 className="text-5xl">Welcome Traveller</h1>
+        <div className="flex w-full flex-col items-start justify-start space-y-4">
+          <h2 className="text-3xl font-light">I'm Paul Jasper.</h2>
+
+          <Button
+            variant="link2"
+            size="sm"
+            className="rounded-full [&_svg]:!size-6"
+            asChild
+          >
+            <a
+              href="https://twitter.com/pauljasperdev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-lg font-extralight"
+            >
+              <Xicon />
+              pauljasperdev
+            </a>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
