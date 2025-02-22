54996a51840854a9d48636094d48e1d9c794f351
pauljaspersahr <pauljasper.sahr@gmail.com>
2025-02-22 14:12:56 +0100

    fix lint errors

--- src/app/_components/welcome.tsx
+++ src/app/_components/welcome.tsx
@@ -7,7 +7,7 @@ export default function Welcome() {
       <div className="flex flex-col items-start justify-center space-y-8">
         <h1 className="text-5xl">Welcome Traveller</h1>
         <div className="flex w-full flex-col items-start justify-start space-y-4">
-          <h2 className="text-3xl font-light">I'm Paul Jasper.</h2>
+          <h2 className="text-3xl font-light">{"I'm Paul Jasper."}</h2>
 
           <Button
             variant="link2"
