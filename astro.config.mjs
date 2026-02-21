b6e3e2587b9313939786b9638aa6b100cb95e5e4
Paul Jasper <mail@pauljasper.dev>
2026-02-21 22:03:25 +0100

    feat(meta): add OG + Twitter Card meta tags and generate OG image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://pauljasper.dev',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
